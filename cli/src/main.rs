10b59e9e50788ca6f517ed2ea7616676162ff956
Wilson Lin <code@wilsonl.in>
2022-10-04 22:44:46 +1100

    Formatting

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,14 +1,14 @@
 use parse_js::parse;
 use parse_js::parse::toplevel::TopLevelMode;
 use parse_js::serialize::serialize_ast;
-use std::io::Read;
 use std::io::stdin;
 use std::io::stdout;
+use std::io::Read;
 
 fn main() {
-  let mut source = Vec::new();
-  stdin().read_to_end(&mut source).expect("read from stdin");
-  let parsed = parse(source, TopLevelMode::Global).expect("parse");
-  let ast = serialize_ast(&parsed.node_map, parsed.top_level_node_id);
-  serde_json::to_writer(stdout(), &ast).expect("write to stdout");
+    let mut source = Vec::new();
+    stdin().read_to_end(&mut source).expect("read from stdin");
+    let parsed = parse(source, TopLevelMode::Global).expect("parse");
+    let ast = serialize_ast(&parsed.node_map, parsed.top_level_node_id);
+    serde_json::to_writer(stdout(), &ast).expect("write to stdout");
 }
