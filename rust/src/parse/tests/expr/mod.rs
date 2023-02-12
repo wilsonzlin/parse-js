9c8ade78af32efbafddf57e26d49c82fedf3781f
Wilson Lin <code@wilsonl.in>
2023-02-13 01:19:41 +1100

    Fix tests

--- rust/src/parse/tests/expr/mod.rs
+++ rust/src/parse/tests/expr/mod.rs
@@ -1,22 +1,28 @@
 use crate::lex::Lexer;
-use crate::parse::expr::parse_expr;
-use crate::parse::parser::Parser;
 use crate::parse::pattern::ParsePatternSyntax;
-use crate::serialize::serialize_ast;
+use crate::parse::ParseCtx;
+use crate::parse::Parser;
+use crate::session::Session;
+use crate::symbol::Scope;
+use crate::symbol::ScopeType;
 use crate::token::TokenType;
 use crate::util::test::evaluate_test_input_files;
 use serde_json::Value;
 
 fn parse_expr_and_serialize(input: Vec<u8>) -> Value {
-  let mut parser = Parser::new(Lexer::new(input));
-  let scope = parser.create_global_scope();
-  let syntax = ParsePatternSyntax {
-    await_allowed: true,
-    yield_allowed: true,
+  let session = Session::new();
+  let mut parser = Parser::new(Lexer::new(&input));
+  let scope = Scope::new(&session, None, ScopeType::Global);
+  let ctx = ParseCtx {
+    scope,
+    session: &session,
+    syntax: ParsePatternSyntax {
+      await_allowed: true,
+      yield_allowed: true,
+    },
   };
-  let node_id = parse_expr(scope, &mut parser, TokenType::Semicolon, &syntax).unwrap();
-  let (node_map, _) = parser.take();
-  serialize_ast(&node_map, node_id)
+  let node = parser.parse_expr(ctx, TokenType::Semicolon).unwrap();
+  serde_json::to_value(&node).unwrap()
 }
 
 #[test]
--- rust/src/parse/tests/stmt/mod.rs
+++ rust/src/parse/tests/stmt/mod.rs
@@ -1,21 +1,27 @@
 use crate::lex::Lexer;
-use crate::parse::parser::Parser;
 use crate::parse::pattern::ParsePatternSyntax;
-use crate::parse::stmt::parse_stmt;
-use crate::serialize::serialize_ast;
+use crate::parse::ParseCtx;
+use crate::parse::Parser;
+use crate::session::Session;
+use crate::symbol::Scope;
+use crate::symbol::ScopeType;
 use crate::util::test::evaluate_test_input_files;
 use serde_json::Value;
 
 fn parse_stmt_and_serialize(input: Vec<u8>) -> Value {
+  let session = Session::new();
   let mut parser = Parser::new(Lexer::new(&input));
-  let scope = parser.create_global_scope();
-  let node_id = parse_stmt(scope, &mut parser, &ParsePatternSyntax {
-    await_allowed: true,
-    yield_allowed: true,
-  })
-  .unwrap();
-  let (node_map, _) = parser.take();
-  serialize_ast(&node_map, node_id)
+  let scope = Scope::new(&session, None, ScopeType::Global);
+  let ctx = ParseCtx {
+    scope,
+    session: &session,
+    syntax: ParsePatternSyntax {
+      await_allowed: true,
+      yield_allowed: true,
+    },
+  };
+  let node = parser.parse_stmt(ctx).unwrap();
+  serde_json::to_value(&node).unwrap()
 }
 
 #[test]
